8ed57220d1ff89610fb67a5cc4d482d42c3a82cd
shunsuke <wos.shun@gmail.com>
2021-05-22 15:50:24 +0900

    paginate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,16 +18,16 @@ class PostController extends Controller
         $q = \Request::query();
 
         if (isset($q['category_id']) == true) {
-            $posts = Post::latest()->where('category_id', $q['category_id'])->get();
+            $posts = Post::latest()->where('category_id', $q['category_id'])->paginate(5);
+            $posts->load('category', 'user');
+
+            return view('posts.index', ['posts' => $posts]);
+        } else {
+            $posts = Post::latest()->paginate(5);
             $posts->load('category', 'user');
 
             return view('posts.index', ['posts' => $posts]);
         }
-
-        $posts = Post::all();
-        $posts->load('category', 'user');
-
-        return view('posts.index', ['posts' => $posts]);
 
     }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,25 +3,26 @@
 @section('content')
 <div class="card-header">Board</div>
 <div class="card-body">
-@if (session('status'))
-    <div class="alert alert-success" role="alert">
-        {{ session('status') }}
-    </div>
-@endif
+  @if (session('status'))
+      <div class="alert alert-success" role="alert">
+          {{ session('status') }}
+      </div>
+  @endif
 
-@foreach($posts as $post)
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">{{ $post->title }}</h5>
-      <h5 class="card-title">カテゴリー:
-      <a href="{{ route('posts.index', ['category_id' => $post->category_id]) }}">{{ $post->category->category_name }}</a></h5>
-      <h5 class="card-title">投稿者:
-      <a href="{{ route('users.show', $post->user_id) }}">{{ $post->user->name }}</h5></a>
-      <p class="card-text">{{ $post->content }}</p>
-      <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">詳細</a>
+  @foreach($posts as $post)
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{ $post->title }}</h5>
+        <h5 class="card-title">カテゴリー:
+        <a href="{{ route('posts.index', ['category_id' => $post->category_id]) }}">{{ $post->category->category_name }}</a></h5>
+        <h5 class="card-title">投稿者:
+        <a href="{{ route('users.show', $post->user_id) }}">{{ $post->user->name }}</h5></a>
+        <p class="card-text">{{ $post->content }}</p>
+        <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">詳細</a>
+      </div>
     </div>
-  </div>
-@endforeach
+  @endforeach
+  {{ $posts->links() }}
 </div>
 
 @endsection
